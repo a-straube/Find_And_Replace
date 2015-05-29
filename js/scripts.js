c5f615cde8818788859173a99bd863accd664310
Axel <kurtastraube@gmail.com>
2015-05-29 12:17:33 -0700

    Change function to findAndReplace, pass corresponding spec, and have basic webpage form built.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-var contains = function(sentence, word) {
-  return true
+var findAndReplace = function(sentence, findWord, replacementWord) {
+  var splitSentence = sentence.split(' ');
+  var findWord = findWord;
+  var replacementWord = replacementWord;
+
+  for (var index = 0; index < splitSentence.length; index += 1) {
+    if (splitSentence[index] === findWord) {
+      splitSentence[index] = replacementWord;
+    };
+  };
+
+  return splitSentence.join(' ');
 };
+
+$(document).ready(function() {
+  $("form#far").submit(function(event) {
+    var sentence = $("input#sentence").val();
+    var oldWord = $("input#oldword").val();
+    var newWord = $("input#newword").val();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
-describe('contains', function() {
-  it('returns true if a word is contained in a sentence', function() {
-    expect(contains("once upon a time", 'time')).to.equal(true);
+describe('findAndReplace', function() {
+  it('replaces a searched word in a sentence with a replacement word', function() {
+    expect(findAndReplace("Hello world", "world", "friend")).to.equal("Hello friend");
   });
 });
